45c1b3d629cfe2c48b97df2039a53055f582c289
digiroiio-lgtm <digiroiio@gmail.com>
2025-12-11 13:33:21 +0300

    .

--- flightpure/vite.config.ts
+++ flightpure/vite.config.ts
@@ -3,4 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: "0.0.0.0",
+    port: 3000,
+    allowedHosts: ["airprosaas-vj5e9.ondigitalocean.app"],
+  },
 });
